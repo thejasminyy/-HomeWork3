ad670b25a62adb7e3a749b63c1a1126a1352c70c
Jasmin Chen <oo13847@gmail.com>
2021-07-02 23:33:12 +0800

    first commit

--- js/products.js
+++ js/products.js
@@ -1,25 +1,65 @@
 const url = 'https://vue3-course-api.hexschool.io/'; // 站點
 const path = 'jasmin'; // 個人api
+// import pagination from '../components/pagation.js';
+import pagination from './components/pagation.js';
+
+// import productList from '../components/productList.js';
+import productList from './components/productList.js';
+// import productModalComponent from '../comments/productModal.js';
+import productModalComponent from './comments/productModal.js';
+
+// import deleteModalComponent from '../comments/deleteModal.js'
+import deleteModalComponent from './comments/deleteModal.js'
+
+
 
  //預設變數
   let productModal = null;
   let delProductModal = null;
 
-Vue.createApp({
+const app = Vue.createApp({
   data() {
     return  {
       products:[],
       temporaryProuct:{},
+      totalPageNum:0,
+      currentPage: 1,
       deleteProductId:"",
     }
   },
+  components: {
+    pagination, 
+    productList,
+    productModalComponent,
+    deleteModalComponent   
+  },
   methods: {
-    getProduct() {
-      axios.get(`${url}api/${path}/admin/products`).then((res)=>{
+    getProduct(page) {
+      axios.get(`${url}api/${path}/admin/products? page=${page}`).then((res)=>{
         this.products = res.data.products;
+        //取得總頁數
+        this.totalPageNum = res.data.pagination.total_pages;      
         console.log(res.data.products);
       })
     },
+    getPageNum(pageNum) {
+      if (pageNum == "pre") {
+        //如果不是在第一頁
+        if(this.currentPage !==1){
+           this.currentPage -= 1; 
+        }      
+      } else if(pageNum == "next") {
+        //如果不是在最後一頁
+        if (this.currentPage !== this.totalPageNum ){
+        this.currentPage += 1;
+        }        
+      } else{
+        this.currentPage = pageNum;
+      }     
+    },
+    getProductId(id,pattern){     
+      this.showProductModal(pattern,id);       
+    }, 
     showProductModal(pattern,direction) {
       if(pattern == 'create') {
         //add
@@ -37,20 +77,18 @@ Vue.createApp({
         delProductModal.show();
       }
     },
-    modifyProduct(id) {
-      //get modal data
-      const data = {
-        ...this.temporaryProuct,
-        origin_price: parseInt(this.temporaryProuct.origin_price),
-        price: parseInt(this.temporaryProuct.price)
-      };
-      if(id == undefined) {
-        this.postNewProduct(data);
+    getModifyProduct(product){
+      //利用product是否有id來判定是編輯還是新增
+      if(product.id){
+        //編輯
+        this.putProduct(product.id,product);
       }else{
-        this.putProduct(id,data);
+        //新增
+      this. postNewProduct(product);
       }
-    },
-    deleteProduct(id) {
+     },  
+    deleteProduct() {
+      const id = this.deleteProductId;
       axios.delete(`${url}api/${path}/admin/product/${id}`).then(res=>{
         if(!res.data.success) {
           alert(res.data.message);  
@@ -101,4 +139,5 @@ Vue.createApp({
     delProductModal = new bootstrap.Modal(document.querySelector('#delProductModal'));
   }
 
-}).mount('#app');
+});
+app.mount('#app');
